939f2bfba35df5d9c192e67c4c3b1fefce98c0f3
florentina <florentinavojvoda9@gmail.com>
2021-03-09 20:49:29 +0100

    hw added

---
+++ 01_syntax.js
@@ -0,0 +1,68 @@
+/*Detyra1 */
+
+let name = prompt('What is your name?');
+console.log('Hello, ' + name);
+
+
+/*Detyra2 */
+
+// const currentYear = 2021;
+// let birthYear = parseInt(prompt('What is your birth year?'));
+// let age = currentYear - birthYear;
+// console.log('You are ' + age + ' years old.');
+
+
+/*Detyra3 */
+
+// let sideLength = parseInt(prompt('Write length of a side of a square: '));
+// let p = 4 * sideLength;
+// console.log('The perimter of a square with a side length ' + sideLength + ' is ' + p + '.');
+
+
+/*Detyra4 */
+
+// const PI = 3.14159265359;
+// let radius = parseFloat(prompt('Write radius of a circle: '));
+// let p = parseFloat(2) * PI * radius;
+// console.log('The perimter of a circle with radius = ' + radius + ' is ' + p + '.');
+
+
+/*Detyra5 */
+
+// let distance = parseInt(prompt('Write distance between two cities:'));
+// let time = parseFloat(prompt('Write time you want to cover:') / 60);
+// let velocity = parseFloat(distance/time);
+// console.log('The speed needed to be on time is ' + velocity);
+
+
+/*Detyra6 */
+
+// let exchange = 0.84;
+// let USD = parseFloat(prompt('Write how many USD do you want to convert:'));
+// let euros = USD * exchange;
+// console.log(USD + ' dollars are equal to ' + euros + ' euros.');
+
+
+/*Detyra7 */
+
+// let gb = parseInt(prompt('Write memory in GB:'))*1024;
+// let result1 = parseInt(gb / 820);
+// console.log(gb + ' GB can store ' + result1 + ' of 820MB files');
+
+
+/*Detyra8 */
+
+// let amount = parseInt(prompt('Write amount of money in wallet:'));
+// let chocolate = parseInt(prompt('Write price of chocolate:'));
+// let result1 = amount % chocolate;
+// let result2 = parseInt(amount / chocolate);
+// console.log('You can buy ' + result2 + ' chocolates and you have ' + result1 + ' euros left');
+
+
+/*Detyra9 */
+
+// let sumOfBankDeposits = parseFloat(prompt('Write the sum of bank deposits for 2 months:'));
+// let interestRate = parseFloat(5/100);
+// let termOfLoan = parseFloat(2/12);
+// let sumOfInterest = interestRate * sumOfBankDeposits * termOfLoan;
+// console.log('Sum of interest of bank deposits of ' + sumOfBankDeposits + ' for 2 months with a yearly interest rate of 5% is ' + sumOfInterest);
